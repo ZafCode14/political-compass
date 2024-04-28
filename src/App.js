be53a839d9fe4e7b3910983e9c295fc30af8529a
ZafCode14 <zafarwp14@gmail.com>
2024-04-28 21:28:34 +0300

    removed-footer-header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
-import Header from './Header';
-import Footer from './Footer';
 import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
 
 function App() {
     return (
     <div className="App">
         <BrowserRouter>
-            <Header/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
-                <Route path="/about" element={<About/>}/>
-                <Route path="/contact" element={<Contact/>}/>
             </Routes>
-            <Footer/>
         </BrowserRouter>
     </div>
     ); 
